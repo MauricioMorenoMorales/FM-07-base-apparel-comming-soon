dc4afb2af52633eafc44384ce21eb38868b73f45
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-09 07:29:08 -0500

    Functionallity completed

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const $textArea = document.querySelector('.description__input__text-area');
+const $button = document.querySelector('.description__input__button');
+const $errorMessage = document.querySelector(
+	'.description__input__error-message',
+);
+const emailRegex =
+	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+$button.addEventListener('click', event => {
+	if (emailRegex.test($textArea.value)) {
+		$textArea.classList.remove('active');
+		$errorMessage.classList.remove('active');
+		$textArea.value = '';
+	} else {
+		$textArea.classList.add('active');
+		$errorMessage.classList.add('active');
+	}
+});
